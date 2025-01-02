1f866928ff5420aa929d4bb5aecbef2c311e4439
Timur4ik <tim.skobenko@mail.ru>
2025-01-03 01:38:18 +0300

    feat(routing): added routing via react-router-dom

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,13 @@
+import { InternalRouterProvider } from "@/shared";
+import { RouterProvider } from "react-router-dom";
+import { createRouter } from "./router";
+
 function App() {
   return (
-    <main>
-      Hello world
-    </main>
-  )
+    <InternalRouterProvider>
+      <RouterProvider router={createRouter()} />
+    </InternalRouterProvider>
+  );
 }
 
-export default App
+export default App;
